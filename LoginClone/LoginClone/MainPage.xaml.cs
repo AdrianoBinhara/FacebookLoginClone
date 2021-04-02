3ce51fec4fe85783019e418b43721e1c5a0832e5
Adriano <adriano.goncalves@restaurantemadero.com.br>
2021-04-02 18:21:46 -0300

    Facebook Clone Solution

---
+++ LoginClone/LoginClone/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LoginClone
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void TapGestureRecognizer_Tapped(Object sender, EventArgs e)
+        {
+            PasswordFlex.TranslationX = Width;
+            PasswordFlex.IsVisible = true;
+
+            ImageFlex.TranslationY = 100;
+
+            labelName.FadeTo(0, 250);
+            await ImageFlex.TranslateTo(ImageFlex.TranslationX, 0, 250, easing: Easing.SinIn);
+
+            await PasswordFlex.TranslateTo(0, PasswordFlex.TranslationY, 250, easing: Easing.SinIn);
+        }
+    }
+}
